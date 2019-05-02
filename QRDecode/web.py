2ab5551ab3330199e836e9b89a4c866eda44f39b
William Markito <markito@redhat.com>
2019-05-01 23:43:12 -0500

    Initial import

---
+++ QRDecode/web.py
@@ -0,0 +1,56 @@
+#!/bin/python
+#########
+## QRReader Payment
+#########
+from money.money import Money
+from flask import Flask, flash, request, redirect, url_for
+from pyzbar.pyzbar import decode
+from PIL import Image, ImageEnhance
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+'''
+    Return data field value from QRcode
+'''
+def _decode(image):
+    local_image = Image.open(image)
+    local_image = ImageEnhance.Contrast(local_image).enhance(4.0)
+    decodedObjects = decode(local_image)
+    # only for demo
+    for obj in decodedObjects:
+        print('Type : ', obj.type)
+        print('Data : ', obj.data,'\n')  
+    if (decodedObjects[0].data):
+        return decodedObjects[0].data 
+    else:
+        flash("No data found on this QRcode.")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file and allowed_file(file.filename):
+            return _decode(file)
+    # don't this at home - use templates
+    return '''
+    <!doctype html>
+    <title>Payment System - Upload the QR Code</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <h1>QR Payment System 💸💳</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file accept="image/*;capture=camera">
+      <input type=submit value="Pay!">
+    </form>
+    '''
---
+++ QREncode/web.py
@@ -0,0 +1,40 @@
+#!/bin/python
+#########
+## QRGenerator
+#########
+
+from flask import Flask, send_file
+from flask import request
+from io import BytesIO
+from money.money import Money
+from money.currency import Currency
+from money.money import Money
+import traceback
+import logging
+import qrcode
+
+app = Flask(__name__)
+DEFAULT_ID = "-1111111111"
+
+def _serve_pil_image(pil_img):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
+@app.route('/encode', methods=['GET', 'POST'])
+def encode():
+    source = request.args.get('source', default = "Red Hat Summit 2019", type = str)
+    id = request.args.get('id', default = DEFAULT_ID, type = str)
+    amount = request.args.get('amount', default = "0.0", type = str)
+    try:
+        if (id == DEFAULT_ID):
+            img = qrcode.make(source + ";" + id + ";" + amount)
+            return _serve_pil_image(img)
+        else: 
+            money_amount = Money(amount, Currency.USD)
+            img = qrcode.make(source + ";" + id + ";" + money_amount.format('en_US'))
+        return _serve_pil_image(img)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        return "Error generating QR code."
